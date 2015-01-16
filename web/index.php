b29b8454d63750a238b58d1bbfe6ecb85c62913d
dil <dil@crossovernepal.com>
2015-01-15 22:55:17 -0500

    deleted copy

--- web/index.php
+++ web/index.php
@@ -10,24 +10,4 @@ echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 >>>>>>> master<br> <br> <br> <br>
 here comes the master
 writeMsg();
-?>
-=======
-<br><br> <br> <br> <br>the phh is pre processor hypertext php
-
-<?php
-$cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
-?>
->>>>>>> master<br> <br> <br> <br>
-here comes the master
-?>
-=======
-<br><br> <br> <br> <br>the phh is pre processor hypertext php
-
-<?php
-$cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
-?>
->>>>>>> master<br> <br> <br> <br>
-here comes the master
 
